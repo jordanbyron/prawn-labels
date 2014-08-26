5fcd2f356c279f384bef550308f31f70c250951c
Jordan Byron <jordan.byron@gmail.com>
2014-08-26 09:14:29 -0400

    Move examples into smoke tests for easy debugging
    These tests are super basic and don't actually test each feature, but
    they do let us know that all of the examples run without raising any
    errors and produce PDFs

--- examples/example_helper.rb
+++
@@ -1,5 +0,0 @@
-$LOAD_PATH.unshift(File.expand_path('../lib', File.dirname(__FILE__)))
-require 'rubygems'
-require 'prawn/core'
-require 'prawn/labels'
-Prawn.debug = true
---
+++ test/test_helper.rb
@@ -0,0 +1,7 @@
+require 'bundler/setup'
+require 'minitest/autorun'
+require 'minitest/spec'
+require 'purdytest'
+
+require 'prawn'
+require_relative '../lib/prawn/labels'
