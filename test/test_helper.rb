750244112dcb5bf2a6b2d3dbaa87ba52f81dce23
Jordan Byron <jordan.byron@gmail.com>
2014-08-26 09:17:34 -0400

    Add DEBUG option to visually debug test PDFs

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,3 +5,11 @@ require 'purdytest'
 
 require 'prawn'
 require_relative '../lib/prawn/labels'
+
+class MiniTest::Spec
+  after do
+    if ENV['DEBUG']
+      `open #{pdf_path}`
+    end
+  end
+end
